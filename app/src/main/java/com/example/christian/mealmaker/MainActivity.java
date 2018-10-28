b8d9e633b8674228c5dc9dc6c39be5e71494f4cc
christiandomantay <christianremegio.domantay.iics@ust.edu.ph>
2018-10-28 23:28:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/christian/mealmaker/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.christian.mealmaker;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       // Intent i = new Intent(this, MyService.class);
+       // startService(i)
+    }
+    public void process(View v) {
+        Intent i = null, chooser = null;
+      if(v.getId() == R.id.start) {
+            i = new Intent(this, Main2Activity.class);
+            startActivity(i);
+        }
+    }
+
+}
